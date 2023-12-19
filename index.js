e91dd29e0f6384a076f4f5dbfb410abb0f096d98
Alif Panglima Nurda <alifnurda@gmail.com>
2023-12-19 17:58:03 +0700

    added route analyse api

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 var express = require('express');
 var cors = require('cors');
+const bodyParser = require('body-parser')
+const multer = require('multer')
+const analyse = multer({ dest: '/analyse' })
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/analyse', analyse.single('upfile'), function (req, res, next) {
+  const file = req.file.upfile
+  res.file(file)
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
